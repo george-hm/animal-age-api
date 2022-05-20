252aa208630bc8015dd4263b02e94ab544b3ee34
George <georgehm@pm.me>
2022-05-20 01:21:13 +0100

    Setup serverless config

---
+++ src/shared/lib.js
@@ -0,0 +1,28 @@
+/**
+ * Returns an AWS-lambda proxy response
+ * Use this for returning and ending a lambda function
+ *
+ * @static
+ * @param {number} status     The status code to return
+ * @param {any}    [body]     A body to return
+ * @param {string} [location] A URL to redirect to - if correct status code
+ * @returns
+ */
+export function createResponse(status, body, location) {
+    const response = {
+        statusCode: status,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            Location: '',
+        },
+        body: JSON.stringify(body) || '{}',
+    };
+
+    if (location) {
+        response.headers.Location = location;
+    } else {
+        delete response.headers.Location;
+    }
+
+    return response;
+}
